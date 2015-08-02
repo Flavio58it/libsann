edb7e96f1f30ebb9b755f564d2924e63cc911f96
dwaita <matteo.tosato7@gmail.com>
2015-08-02 11:47:27 +0200

    Implement training set export function

--- libsannNETWorkbenchToolkit/Utils/Export.cs
+++ libsannNETWorkbenchToolkit/Utils/Export.cs
@@ -4,16 +4,51 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using libsannNETWorkbenchToolkit.Set;
 
 namespace libsannNETWorkbenchToolkit.Utils
 {
     internal class Export
     {
-        public static void SetToFile(Stream stream)
+        /// <summary>
+        /// Print training set to file
+        /// </summary>
+        /// <param name="stream">Stream of file</param>
+        /// <param name="set">Training set</param>
+        public static void SetToFile(Stream stream, SetModel set)
         {
             using (StreamWriter w = new StreamWriter(stream, Encoding.UTF8))
             {
-                // TODO: implemento esportazione
+                w.WriteLine("# Set exported {0}", DateTime.Now.ToLongDateString());
+                var inMatrixCount = set.inMatrix.Count();
+                var outMatrixCount = set.outMatrix.Count();
+
+                w.WriteLine("i:{0}", inMatrixCount);
+                w.WriteLine("v:{0}", outMatrixCount);
+
+                w.WriteLine("[input]");
+                WriteMatrix(set.inMatrix, w);
+
+                w.WriteLine("[output]");
+                WriteMatrix(set.outMatrix, w);
+
+                w.WriteLine("[inValidation]");
+                WriteMatrix(set.inValMatrix, w);
+
+                w.WriteLine("[outValidation]");
+                WriteMatrix(set.outValMatrix, w);
+            }
+        }
+
+        protected static void WriteMatrix(double[][] matrix, StreamWriter writer)
+        {
+            foreach (double[] row in matrix)
+            {
+                foreach (double value in row)
+                {
+                    writer.Write("{0};", value);
+                }
+                writer.WriteLine(); // New line
             }
         }
     }
